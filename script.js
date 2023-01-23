4d576cf0b663ca0e3abbfd9f1239a2de2482f1bb
Nelson de Barros <nelsinho.527@gmail.com>
2023-01-23 11:42:47 -0300

    basic template done

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const container = document.querySelector('#container');
 
-for (i=0; i<255;i++){
+for (i=0; i<256;i++){
     container.appendChild(document.createElement('div'))
 
 }
+
+
+
+let number;
+const buttons = document.querySelectorAll('.button');
+buttons.forEach((button)=>{button.addEventListener('click',()=> {
+    do {
+        number = window.prompt("Enter a number below 100");
+    }
+    while (number > 100);
+
+
+    
+})
+});
