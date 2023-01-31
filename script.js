20fbd484a9bf0b45a3308e0667edf671b858a001
Nelson de Barros <nelsinho.527@gmail.com>
2023-01-31 08:00:27 -0300

    prettier

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-const container = document.querySelector('#container');
+const container = document.querySelector("#container");
 
-for (i=0; i<256;i++){
-    container.appendChild(document.createElement('div'))
-
+for (i = 0; i < 256; i++) {
+  container.appendChild(document.createElement("div"));
 }
 
 function removeDivs(parent) {
-    while(parent.firstChild){
-        parent.removeChild(parent.firstChild)
-    }
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
 }
 
 let number;
-const buttons = document.querySelectorAll('.button');
-buttons.forEach((button)=>{button.addEventListener('click',()=> {
+const buttons = document.querySelectorAll(".button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
     do {
-        number = window.prompt("Enter a number below 100");
-    }
-    while (number > 100);
+      number = window.prompt("Enter a number below 100");
+    } while (number > 100);
     removeDivs(container);
-    container.setAttribute('style', `grid-template-columns: repeat(${number}, 2fr); grid-template-rows: repeat(${number}, 2fr);`)
-    for (i=0; i<(number*number);i++){
-        container.appendChild(document.createElement('div'))
-    
+    container.setAttribute(
+      "style",
+      `grid-template-columns: repeat(${number}, 2fr); grid-template-rows: repeat(${number}, 2fr);`
+    );
+    for (i = 0; i < number * number; i++) {
+      container.appendChild(document.createElement("div"));
     }
-    
-})
+  });
 });
